e70b6edb3a86ffb2d5d8ca45a62352d5f393aa2c
liuguoquan <liuguoquan727@126.com>
2017-07-29 13:21:08 +0800

    迁移

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/liuguoquan/workspace/CLionProjects/opencv-study
+# Install script for directory: /Users/liuguoquan/workspace/study/opencv3-study
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -35,5 +35,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/liuguoquan/workspace/CLionProjects/opencv-study/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/liuguoquan/workspace/study/opencv3-study/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
